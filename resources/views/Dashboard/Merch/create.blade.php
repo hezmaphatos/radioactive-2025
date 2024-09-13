92b0ad1a61b12125c379ac2744ec24e212181ad2
Merhandes Gunawan <merhandes.gunawan@student.umn.ac.id>
2024-09-13 15:11:59 +0700

    Update create.blade.php

--- resources/views/Dashboard/Merch/create.blade.php
+++ resources/views/Dashboard/Merch/create.blade.php
@@ -74,7 +74,7 @@
               </div>
               <div>
                 <div>
-                    <label for="image" class="mb-3 block text-sm font-medium text-black dark:text-white">
+                    <label for="image" class="mb-3 block text-sm font-medium text-black">
                         Cover Image
                     </label>
                     <input type="file" id="image" name="image" class="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter @error('image') border-red-500 @enderror" onchange="previewImage()"/>

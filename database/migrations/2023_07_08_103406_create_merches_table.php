8b6cc5c41fbff8b794b8919503c0399d82c72099
ptikelompok8 <willbertlian@gmail.com>
2025-07-13 17:17:30 +0700

    merch type

--- database/migrations/2023_07_08_103406_create_merches_table.php
+++ database/migrations/2023_07_08_103406_create_merches_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image');
             $table->bigInteger('price');
             $table->integer('stock');
+            $table->string('type'); // 'merch' or 'preorder'
             $table->timestamps();
         });
     }

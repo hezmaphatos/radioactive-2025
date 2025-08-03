5b3c811562f8bf3ad327d3677a86bf06a87e5d58
Juan Azriel <juanazriel.id@gmail.com>
2025-08-03 18:57:34 +0700

    Ikon dan teks menu menyala saat halaman terbuka

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <div id="navbar" x-data="{ isOpen: false }"
-    class="font-royale-serif sticky top-0 z-50 w-full bg-[#0e0e0e] bg-opacity-80 backdrop-blur-md shadow-md py-8 px-4 lg:px-12 xl:px-20">
+    class="font-royale-serif sticky top-0 z-50 w-full bg-[#20661c] backdrop-blur-md shadow-md py-8 px-4 lg:px-12 xl:px-20">
 
     <div class="max-w-7xl mx-auto flex items-center justify-between relative">
 
@@ -29,8 +29,9 @@
                 @foreach ($menus as $menu)
                     <div class="relative group flex flex-col items-center text-white">
                         <a href="{{ $menu['href'] }}" class="group hover:text-[#f6e79c] transition duration-300 text-xl">
-                            <i
-                                class="fas {{ $menu['icon'] }} text-white transform group-hover:scale-125 group-hover:text-[#f6e79c] transition duration-300"></i>
+                            <i class="fas {{ $menu['icon'] }}
+                                {{ (Request::is('/') && $menu['href'] == '/') || Request::is(ltrim($menu['href'], '/')) ? 'text-[#f6e79c]' : 'text-white' }}
+                                transform group-hover:scale-125 group-hover:text-[#f6e79c] transition duration-300"></i>
                         </a>
                         <div
                             class="absolute top-[2.5rem] px-3 py-1 bg-[#f6e79c] text-black text-base rounded shadow-md opacity-0 group-hover:opacity-100 transition duration-300 whitespace-nowrap">
@@ -71,9 +72,13 @@
     <div x-show="isOpen" @click.away="isOpen = false"
         class="lg:hidden mt-8 w-full bg-[#111] text-white rounded-md shadow-lg p-4 z-50">
         @foreach ($menus as $menu)
+            @php
+                $isActive = (Request::is('/') && $menu['href'] == '/') || Request::is(ltrim($menu['href'], '/'));
+            @endphp
             <div class="flex items-center space-x-3 mb-4">
-                <i class="fas {{ $menu['icon'] }} w-5 h-5 text-white"></i>
-                <a href="{{ $menu['href'] }}" class="text-sm font-ltmuseumbold text-white no-underline">
+                <i class="fas {{ $menu['icon'] }} w-5 h-5 {{ $isActive ? 'text-[#f6e79c]' : 'text-white' }}"></i>
+                <a href="{{ $menu['href'] }}"
+                    class="text-sm font-ltmuseumbold no-underline {{ $isActive ? 'text-[#f6e79c]' : 'text-white' }}">
                     {{ $menu['desc'] }}
                 </a>
             </div>
